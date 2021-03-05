bdd6e6f5abb8d1bd583514b3b74ff8ee02ad3b00
denisfeo <dfeoktistov523@icloud.com>
2021-03-05 18:23:23 +0300

    First interface(adaptive) was added.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from MNISTDataReader import read_info
+from PyQt5.QtWidgets import QApplication
+from MainWindow import MainWindow
+import sys
+
+
+class App(QApplication):
+    def __init__(self, args):
+        super().__init__(args)
+
+        self.user_screen_geometry = self.desktop().screenGeometry()
+        self.main_window = MainWindow(self)
+
+    def show(self):
+        self.main_window.show()
 
 
 if __name__ == "__main__":
-    n = int(input("Enter number: "))
-    print(read_info(n, mode="training"))
+    app = App(sys.argv)
+    app.show()
+    sys.exit(app.exec())
