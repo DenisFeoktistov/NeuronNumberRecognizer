fc10b9211dc7fb3b6ba3a1bc61551d29decb8130
denisfeo <dfeoktistov523@icloud.com>
2021-03-06 20:07:11 +0300

    Just a little name fix.

--- AppLogicClasses/MainAppResponder.py
+++ AppLogicClasses/MainAppResponder.py
@@ -1,11 +1,11 @@
-from AppLogicClasses.ModeResponders.TrainModeResponder import TrainModeResponder
+from AppLogicClasses.ModeResponders.TrainingModeResponder import TrainModeResponder
 
 
 class MainAppResponder:
     def __init__(self, app):
         self.app = app
 
-        self.train_mode_responder = TrainModeResponder(self)
+        self.training_mode_responder = TrainModeResponder(self)
 
     def start(self):
-        self.train_mode_responder.start()
+        self.training_mode_responder.start()
