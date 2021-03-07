b496975f7134d994abdbe52fa7e496c358466986
denisfeo <dfeoktistov523@icloud.com>
2021-03-07 10:11:44 +0300

    Fourth variant of gradient and little fixes in code.

--- AppFiles/AppInterfaceClasses/SubsidiaryClasses/NeuronWidget.py
+++ AppFiles/AppInterfaceClasses/SubsidiaryClasses/NeuronWidget.py
@@ -50,32 +50,10 @@ class NeuronWidget:
             f"border-radius: {self.radius}px; border: 3px solid black; background: rgb{self.color}")
 
     def get_gradient(self):
-        # if self.value < 0.5:
-        #     r = 0
-        # elif 0.5 < self.value < 0.59:
-        #     r = 255 * (0.59 - self.value) / (0.59 - 0.5)
-        # else:
-        #     r = 255
-        #
-        # if self.value < 0.5:
-        #     g = self.value / 0.5 * 255
-        # else:
-        #     g = (1 - self.value) / 0.5 * 255
-        #
-        # if self.value > 0.5:
-        #     b = 0
-        # else:
-        #     b = (0.5 - self.value) / 0.5 * 255
-        # r = 255
-        # g = (1 - self.value) * 255
-        # b = 0
         r = 235
-        g = (195 - 120) * (1 - self.value) + 120
+        g = (195 - 120) * (1 - self.value) + 150  # Yes, some of values in this formula aren't described,
+        # but it is really hard to do this. All I wanna say is that it is special formula to get orange gradient.
         b = 80
 
-        r, g, b = max(0, r), max(0, g), max(0, b)
-        print("Value: ", self.value)
-        print("R: ", r)
-        print("G: ", g)
-        print("B: ", b)
+        r, g, b = min(255, max(0, r)), min(255, max(0, g)), min(255, max(0, b))  # these min and max just for be sure
         return r, g, b
