5c667c9c005461aefb27c2dfda200ffde819bdc0
denisfeo <dfeoktistov523@icloud.com>
2021-03-14 17:52:08 +0300

    Cleaning MainWindow code.

--- AppFiles/AppInterfaceClasses/MainWindow/MainWindow.py
+++ AppFiles/AppInterfaceClasses/MainWindow/MainWindow.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from PyQt5.QtWidgets import QMainWindow, QSlider, QLabel, QPushButton, QGraphicsDropShadowEffect
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor
+from typing import Any
 
 from AppFiles.AppInterfaceClasses.MainWindow.SubsidiaryClasses.MatrixWidget import MatrixWidget
 from AppFiles.AppInterfaceClasses.MainWindow.MainWindowResponder import MainWindowResponder
@@ -25,9 +25,9 @@ class MainWindow(QMainWindow):
         self.main_app_interface = main_app_interface
         self.responder = MainWindowResponder(self)
 
-        self.width = self.main_app_interface.app.user_screen_geometry.width() * MainWindow.REL_WIDTH
-        self.height = self.main_app_interface.app.user_screen_geometry.height() * MainWindow.REL_HEIGHT
-        self.height = min(self.height, self.width * 2 // 3)
+        self.width = int(self.main_app_interface.app.user_screen_geometry.width() * MainWindow.REL_WIDTH)
+        self.height = int(self.main_app_interface.app.user_screen_geometry.height() * MainWindow.REL_HEIGHT)
+        self.height = min(self.height, int(self.width * 2 // 3))
 
         self.set_up_window()
         self.add_widgets()
@@ -93,7 +93,7 @@ class MainWindow(QMainWindow):
 
     # Further only graphics initialization
     # ------------------------------------
-    def add_widgets(self):
+    def add_widgets(self) -> None:
         self.add_matrix_widget()
 
         self.add_switch_speed_slider()
@@ -117,13 +117,13 @@ class MainWindow(QMainWindow):
 
         self.add_neural_network_info()
 
-    def add_neural_network_info(self):
+    def add_neural_network_info(self) -> None:
         self.add_name_label()
         self.add_iterations_label()
 
     def add_name_label(self):
         self.name_text_label = QLabel(parent=self, text="Network name: ")
-        font_size = int(self.height // 50)
+        font_size = self.height // 50
         self.name_text_label.move(self.width // 100, self.height // 20)
         self.name_text_label.resize(self.width // 12, self.height // 18)
         self.name_text_label.setStyleSheet(f"font-size: {font_size}px")
@@ -133,35 +133,35 @@ class MainWindow(QMainWindow):
         self.name_label.resize(self.width // 12, self.height // 18)
         self.name_label.setStyleSheet(f"font-size: {font_size}px; font-weight: 900")
 
-    def set_name(self, name):
+    def set_name(self, name: str) -> None:
         self.name_label.setText(str(name))
 
-    def set_iterations(self, iterations):
+    def set_iterations(self, iterations: Any) -> None:
         self.iterations_label.setText(str(iterations))
 
-    def add_iterations_label(self):
+    def add_iterations_label(self) -> None:
         self.iterations_text_label = QLabel(parent=self, text="Total iterations: ")
-        font_size = int(self.height // 50)
+        font_size = self.height // 50
         self.iterations_text_label.move(self.name_text_label.x(), self.name_text_label.y() + self.name_text_label.height())
         self.iterations_text_label.resize(self.name_text_label.width(), self.name_text_label.height())
         self.iterations_text_label.setStyleSheet(f"font-size: {font_size}px")
 
         self.iterations_label = QLabel(parent=self)
-        font_size = int(self.height // 50)
+        font_size = self.height // 50
         self.iterations_label.move(self.iterations_text_label.x() + self.iterations_text_label.width(),
                                         self.iterations_text_label.y())
         self.iterations_label.resize(self.iterations_text_label.width(), self.iterations_text_label.height())
         self.iterations_label.setStyleSheet(f"font-size: {font_size}px; font-weight: 900")
 
-    def add_switch_speed_slider(self):
+    def add_switch_speed_slider(self) -> None:
         self.switch_speed_slider = QSlider(orientation=Qt.Vertical, parent=self)
         self.set_up_slider(self.switch_speed_slider)
 
-    def add_line_width_slider(self):
+    def add_line_width_slider(self) -> None:
         self.line_width_slider = QSlider(orientation=Qt.Vertical, parent=self)
         self.set_up_slider(self.line_width_slider)
 
-    def add_neural_network_answer_label(self):
+    def add_neural_network_answer_label(self) -> None:
         self.neural_network_answer_label = QLabel(parent=self)
         bottom = self.digit_labels[0].geometry().y()
         top = self.digit_labels[MainWindow.OUTPUTS - 1].geometry().y() + self.digit_labels[
@@ -173,10 +173,10 @@ class MainWindow(QMainWindow):
         self.neural_network_answer_label.resize(right - left, top - bottom)
         self.neural_network_answer_label.setAlignment(Qt.AlignCenter)
 
-        font_size = int(self.height // 4 * MainWindow.REL_HEIGHT)
+        font_size = self.height // 4
         self.neural_network_answer_label.setStyleSheet(f"font-size: {font_size}px; color: rgb(0, 0, 0)")
 
-    def add_mode_buttons_and_outlines(self):
+    def add_mode_buttons_and_outlines(self) -> None:
         self.modes_buttons = dict(
             [(mode, QPushButton(parent=self)) for mode in
              ["Automatic training", "Automatic testing", "Manual testing"]])
@@ -205,7 +205,7 @@ class MainWindow(QMainWindow):
             self.modes_buttons[key].setStyleSheet(
                 f"border: 0px solid black; font-size: {font_size}px; color: rgb(0, 0, 0)")
 
-    def add_neuron_widgets_and_digit_labels(self):
+    def add_neuron_widgets_and_digit_labels(self) -> None:
         self.neuron_widgets = [NeuronWidget(parent=self) for _ in range(MainWindow.OUTPUTS)]
         self.arrows_labels = [QLabel(parent=self, text="➔") for _ in range(MainWindow.OUTPUTS)]
         self.digit_labels = [QLabel(parent=self, text=str(i)) for i in range(MainWindow.OUTPUTS)]
@@ -224,17 +224,17 @@ class MainWindow(QMainWindow):
             self.arrows_labels[i].move(self.width // 2 + self.matrix_widget.width // 2 + 2 * self.width // 20,
                                        top_y + step * i)
 
-            font_size1 = int(self.height // 20 * MainWindow.REL_HEIGHT)
+            font_size1 = self.height // 25
             self.arrows_labels[i].setStyleSheet(f"font-size: {font_size1}px; color: rgb(0, 0, 0)")
             self.arrows_labels[i].resize(2 * radius, 2 * radius)
             self.arrows_labels[i].setAlignment(Qt.AlignCenter)
 
-            font_size2 = int(self.height // 15 * MainWindow.REL_HEIGHT)
+            font_size2 = self.height // 20
             self.digit_labels[i].setStyleSheet(f"font-size: {font_size2}px; color: rgb(0, 0, 0)")
             self.digit_labels[i].resize(radius, 2 * radius)
             self.digit_labels[i].setAlignment(Qt.AlignCenter)
 
-    def add_neural_network_answer_text_label(self):
+    def add_neural_network_answer_text_label(self) -> None:
         self.neural_network_answer_text_label = QLabel(parent=self, text="Neural network answer")
         matrix_height = self.matrix_widget.height
         matrix_width = self.matrix_widget.width
@@ -243,50 +243,50 @@ class MainWindow(QMainWindow):
                                                    self.height // 2 - matrix_height // 2 - self.height // 10)
         self.neural_network_answer_text_label.resize(self.width // 2 - matrix_width // 2, self.height // 10)
         self.neural_network_answer_text_label.setAlignment(Qt.AlignCenter)
-        font_size = int(self.height // 20 * MainWindow.REL_HEIGHT)
+        font_size = self.height // 20
         self.neural_network_answer_text_label.setStyleSheet(f"font-size: {font_size}px; color: rgb(0, 0, 0)")
 
-    def add_next_button(self):
+    def add_next_button(self) -> None:
         self.next_button = QPushButton(parent=self, text="Next")
         self.set_up_under_slider_button(self.next_button)
 
-    def add_clear_button(self):
+    def add_clear_button(self) -> None:
         self.clear_button = QPushButton(parent=self, text="Clear")
         self.set_up_under_slider_button(self.clear_button)
 
-    def add_digit_label(self):
+    def add_digit_label(self) -> None:
         self.digit_label = QLabel(parent=self)
 
         matrix_width = self.matrix_widget.width
         matrix_height = self.matrix_widget.height
 
         self.digit_label.setAlignment(Qt.AlignCenter)
-        font_size = int(self.height // 10 * MainWindow.REL_HEIGHT)
+        font_size = self.height // 10
         self.digit_label.setStyleSheet(f"font-size: {font_size}px; border: 1px solid black; color: rgb(0, 0, 0)")
         self.digit_label.move(self.width // 2 - matrix_width // 2,
                               self.height // 2 + matrix_height // 2 + self.height // 15)
         self.digit_label.resize(matrix_width, self.height // 10)
 
-    def add_switch_speed_label(self):
+    def add_switch_speed_label(self) -> None:
         self.switch_speed_label = QLabel(parent=self, text="Switch speed")
         self.set_up_slider_label(self.switch_speed_label)
 
-    def add_line_width_label(self):
+    def add_line_width_label(self) -> None:
         self.line_width_label = QLabel(parent=self, text="Line width")
         self.set_up_slider_label(self.line_width_label)
 
-    def set_up_slider(self, slider: QSlider):
+    def set_up_slider(self, slider: QSlider) -> None:
         matrix_height = self.matrix_widget.height
         matrix_width = self.matrix_widget.width
 
-        width = self.width // 30 * MainWindow.REL_WIDTH
+        width = self.width // 30
         height = matrix_height * 4 // 7
 
-        width1 = height1 = int(self.height // 30 * MainWindow.REL_WIDTH)
+        width1 = height1 = self.height // 30
         border_radius1 = width1 // 2
         margin1 = -border_radius1 // 2
 
-        width2 = int(self.height // 50 * MainWindow.REL_WIDTH)
+        width2 = self.height // 50
         border_radius2 = margin2 = width2 // 2
 
         slider.move(self.width // 2 - matrix_width // 2 - self.width // 15, self.height // 2 - height // 2)
@@ -314,19 +314,18 @@ class MainWindow(QMainWindow):
                             }
                         """)
 
-    def add_matrix_widget(self):
-        width = height = self.height * 0.5
+    def add_matrix_widget(self) -> None:
+        width = height = self.height // 2
 
-        res = dict()
-        x = self.width / 2 - width / 2
-        y = self.height / 2 - height / 2
+        x = self.width // 2 - width // 2
+        y = self.height // 2 - height // 2
         self.matrix_widget = MatrixWidget(parent=self, x=x, y=y, width=width, height=height)
 
     def set_up_under_slider_button(self, button: QPushButton) -> None:
         button.move(self.switch_speed_slider.geometry().x() - self.width // 80,
                     self.height // 2 + self.switch_speed_slider.geometry().y() // 2)
         button.resize(self.switch_speed_slider.geometry().width() + self.width // 40, self.height // 20)
-        font_size = int(self.height // 27 * MainWindow.REL_HEIGHT)
+        font_size = self.height // 35
         button.setStyleSheet(f"font-size: {font_size}px; background: rgb(235, 195, 80); border-radius: 5px; color: rgb(0, 0, 0)")
 
     def set_up_slider_label(self, label: QLabel) -> None:
@@ -334,7 +333,7 @@ class MainWindow(QMainWindow):
         label.move(self.switch_speed_slider.geometry().x() - self.width // 80,
                    self.switch_speed_slider.geometry().y() - self.height // 20)
         label.resize(self.switch_speed_slider.width() + self.width // 40, self.height // 20)
-        font_size = int(self.height // 50 * MainWindow.REL_HEIGHT)
+        font_size = self.height // 60
         label.setAlignment(Qt.AlignCenter)
         label.setStyleSheet(f"font-size: {font_size}px; background-color: transparent; color: rgb(0, 0, 0)")
 
